e6611a4075b5be32b499ab6fcb33cddba9eea2e1
JoeWFisher <joseph.fisher@softwire.com>
2020-05-22 12:37:23 +0100

    Display and add to todo list

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello World!'
+    todos = session.get_items()
+    return render_template('index.html', todos = todos)
+
+@app.route('/index/add', methods=['Post'])
+def add_todo():
+    item = session.add_item(request.form.get('title'))
+    session.save_item(item)
+    return redirect('/index')
 
 if __name__ == '__main__':
     app.run()

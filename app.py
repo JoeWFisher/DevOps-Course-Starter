c095bff5f9e16d4becca7c9b8ee9bdfa067c5e5f
Charlie Cumber <charlie.cumber@softwire.com>
2020-08-03 20:02:15 +0100

    Addition of in progress status

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
 from operator import itemgetter 
 import trello_items as trello
+import view_model
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def index(): 
+def index():
     if request.method == 'POST':
         trello.add_item(request.form.get('title'), request.form.get('description'))
-    return render_template('/index.html', all_items=trello.get_items())
+    return render_template('/index.html', all_items=trello.get_items(), show_archive=False)
 
-@app.route('/toggle-completion/<id>')
-def toggle(id):
-    trello.toggle_status(id)
+@app.route('/archive', methods=['GET'])
+def archive(): 
+    return render_template('/index.html', all_items=trello.get_all_items(), show_archive=True)
+
+@app.route('/set-status/<id>/<status>')
+def set_status(id, status):
+    if status == 'outstanding':
+        trello.set_status_not_started(id)
+    elif status == 'pending':
+        trello.set_status_in_progress(id)
+    elif status == 'completed':
+        trello.set_status_completed(id)
     return redirect('/')
 
 @app.route('/delete/<id>')
--- trello_items.py
+++ trello_items.py
@@ -1,49 +1,82 @@
 import requests
-from trello_config import KEY, TOKEN, BOARD, PENDING, DONE
-import json 
+from trello_config import KEY, TOKEN, BOARD, OUTSTANDING, PENDING, DONE
+from dateutil.parser import parse
+import datetime
+import json
 
 class Item:
     def __init__(self, card):
         self.id = card['id']
         self.title = card['name']
-        if card['idList'] == PENDING:
+        self.statusDatetime = parse(card['due'])
+        if card['idList'] == OUTSTANDING:
             self.status = 'Not Started'
+        elif card['idList'] == PENDING:
+            self.status = 'In Progress'                
         elif card['idList'] == DONE:
             self.status = 'Completed'
         if card['desc'] != 'None':
             self.description = card['desc']
 
 def get_items():
+    all_items = get_all_items()
+    filtered_items = [item for item in all_items if not (item.status == 'Completed' and item.statusDatetime.date() < datetime.date.today())]
+    return filtered_items
+
+def get_all_items():
     response = requests.get(
         f'https://api.trello.com/1/boards/{BOARD}/cards',
         params={'key':KEY, 'token':TOKEN},
     )
     all_items = [Item(item) for item in response.json()]
+    all_items.sort(reverse=True, key=get_status)
     return all_items
 
+def get_status(e):
+    return e.status
+
 def get_item(id):
-    items = get_items()
+    items = get_all_items()
     return next((item for item in items if item.id == id), None)
 
 def add_item(title, description):
-    requests.post(f'https://api.trello.com/1/cards?key={KEY}&token={TOKEN}&idList={PENDING}&name={title}&desc={description}')
+    now = datetime.datetime.now()
+    requests.post(f'https://api.trello.com/1/cards?key={KEY}&token={TOKEN}&idList={OUTSTANDING}&name={title}&desc={description}&due={now}')
     return title
+
+def set_status_not_started(id):
+    item = get_item(id)
+    item.status = 'Not Started'
+    item.date = datetime.datetime.now()
+    save_item(item)
+    return
+
+def set_status_in_progress(id):
+    item = get_item(id)
+    item.status = 'In Progress'
+    item.date = datetime.datetime.now()
+    save_item(item)
+    return
+
+def set_status_completed(id):
+    item = get_item(id)
+    item.status = 'Completed'
+    item.date = datetime.datetime.now()
+    save_item(item)
+    return
 
 def save_item(item):
     if item.status == 'Not Started':
+        list_id = OUTSTANDING
+    elif item.status == 'In Progress':
         list_id = PENDING
     elif item.status == 'Completed':
         list_id = DONE
-    requests.put('https://api.trello.com/1/cards/{id}?key={key}&token={token}&name={name}&idList={listId}'.format(id=item.id, key=KEY, token=TOKEN, name=item.title, listId=list_id))
+    requests.put(
+        'https://api.trello.com/1/cards/{id}?key={key}&token={token}&name={name}&idList={listId}&due={due}'
+        .format(id=item.id, key=KEY, token=TOKEN, name=item.title, listId=list_id, due=item.date)
+    )
     return
-
-def toggle_status(id):
-    item = get_item(id)
-    if item.status == 'Not Started':
-        item.status = 'Completed'
-    elif item.status == 'Completed':
-        item.status = 'Not Started'
-    save_item(item)
 
 def delete_item(id):
     requests.delete('https://api.trello.com/1/cards/{id}?key={key}&token={token}'.format(id=id, key=KEY, token=TOKEN))   

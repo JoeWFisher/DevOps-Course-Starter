e763867886896780e3141faf8fc90c85f8156bf9
JoeWFisher <joseph.fisher@softwire.com>
2021-02-06 18:26:47 +0000

    reverting chromedriver

--- test_system.py
+++ test_system.py
@@ -52,11 +52,11 @@ def test_app():
 
 @pytest.fixture(scope="module")
 def driver():  
-    opts = webdriver.ChromeOptions('./chromedriver') 
+    opts = webdriver.ChromeOptions() 
     opts.add_argument('--headless')   
     opts.add_argument('--no-sandbox') 
     opts.add_argument('--disable-dev-shm-usage')   
-    with webdriver.Chrome(options=opts) as driver:   
+    with webdriver.Chrome('./chromedriver', options=opts) as driver:   
         yield driver 
 
 def test_task_journey(driver, test_app):

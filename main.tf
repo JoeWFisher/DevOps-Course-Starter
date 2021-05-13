ea2409636f8bd5d0eb6b13b2eea76f424ca9c934
JoeWFisher <joseph.fisher@softwire.com>
2021-05-13 18:41:13 +0100

    State backend

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 terraform {
- required_providers {
- azurerm = {
- source = "hashicorp/azurerm"
- version = ">= 2.49"
- }
- }
+  backend "azurerm" {
+    resource_group_name = "SoftwirePilot_JosephFisher_ProjectExercise"
+    storage_account_name = "storageaccountjosfis"
+    container_name = "todoapp-container"
+    key = "8O1ytbiAe65qZQZS3Sx9pDppNbJWYWSTFfklEwcVN3p0ZFbxLn4YZ5I5vv9sejmqFdrFrTDwr39xhJMabdG87Q=="
+    
+  }
 }
+
 provider "azurerm" {
  features {}
 }
@@ -13,7 +15,7 @@ data "azurerm_resource_group" "main" {
  name = "SoftwirePilot_JosephFisher_ProjectExercise"
 }
 resource "azurerm_app_service_plan" "main" {
- name = "${var.prefix}-asp-tf"
+ name = "${var.prefix}-asp-terraform"
  location = var.location
  resource_group_name = data.azurerm_resource_group.main.name
  kind = "Linux"
@@ -50,6 +52,7 @@ resource "azurerm_cosmosdb_mongo_database" "main" {
  name = "${var.prefix}-cosmos-mongo-db"
  resource_group_name = data.azurerm_resource_group.main.name
  account_name = azurerm_cosmosdb_account.main.name
+
  #lifecycle {
   #prevent_destroy = true
  #}
@@ -69,7 +72,6 @@ resource "azurerm_app_service" "main" {
   "client_secret" = var.client_secret
   "FLASK_APP" = "app"
   "FLASK_ENV" = "developement"
-  "PORT" = 5000
   "LOAD_DISABLED" = ""
   "SECRET_KEY" = "real_key"
   "Mongo_db" = "todo_db"
--- variables.tf
+++ variables.tf
@@ -14,3 +14,8 @@ variable "client_secret" {
  description = "The Gihub OAuth app secret"
  default = "1cecb9eeff3882645d3c64c79c56a66e6cc190fd"
 }
+
+variable "access_key" {
+  description = "Storage account access key"
+  default = "8O1ytbiAe65qZQZS3Sx9pDppNbJWYWSTFfklEwcVN3p0ZFbxLn4YZ5I5vv9sejmqFdrFrTDwr39xhJMabdG87Q=="
+}

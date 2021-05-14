24e0a6aac1af87f7320bff41c16d789063cd33f1
JoeWFisher <joseph.fisher@softwire.com>
2021-05-14 14:10:27 +0100

    Moving to tfvars

--- variables.tf
+++ variables.tf
@@ -3,19 +3,15 @@ variable "prefix" {
 }
 variable "location" {
  description = "The Azure location where all resources in this deployment should be created"
- default = "uksouth"
 }
 variable "clientId" {
  description = "The Github client Id env variable"
- default     = "b2aeabde28d46d2b4551"
 }
 
 variable "client_secret" {
  description = "The Gihub OAuth app secret"
- default = "1cecb9eeff3882645d3c64c79c56a66e6cc190fd"
 }
 
 variable "access_key" {
   description = "Storage account access key"
-  default = "8O1ytbiAe65qZQZS3Sx9pDppNbJWYWSTFfklEwcVN3p0ZFbxLn4YZ5I5vv9sejmqFdrFrTDwr39xhJMabdG87Q=="
 }
